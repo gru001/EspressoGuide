822dc19fd4ae7e11d54782a33680e1c221c5f690
pranit <pranitsawant01@gmail.com>
2018-02-01 16:33:53 +0530

    misc ui fixes

--- app/src/main/java/com/example/pranit/espressoguide/MainActivity.java
+++ app/src/main/java/com/example/pranit/espressoguide/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pranit.espressoguide;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,12 +11,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.pranit.espressoguide.basics.BasicActivity;
+import com.example.pranit.espressoguide.custommatcher.CustomMatcherActivity;
+import com.example.pranit.espressoguide.dataadapter.LongListActivity;
+import com.example.pranit.espressoguide.idlingresources.IdlingResourcesActivity;
+import com.example.pranit.espressoguide.intentadavanced.ImageViewerActivity;
+import com.example.pranit.espressoguide.multiwindow.SuggestActivity;
+import com.example.pranit.espressoguide.recyclerview.RecyclerSampleActivity;
+import com.example.pranit.espressoguide.webview.WebViewActivity;
+
 public class MainActivity extends AppCompatActivity implements OnItemClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private RecyclerView recvHome;
     private String[] list = new String[]{"Basic", "Custom Matcher", "Data Adapter",
-            "Idling Resource", "Intent Advanced", "Intent Basic", "Multi-Process", "Multi-window", "RecyclerView", "WebView"};
+            "Idling Resource", "Intent Advanced", "Multi-window", "RecyclerView", "WebView"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +41,34 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
     @Override
     public void onItemClick(int position) {
         Log.i(TAG, "onItemClick: "+position);
+        Intent intent = null;
+        switch (position) {
+            case 0:
+                intent = new Intent(this, BasicActivity.class);
+                break;
+            case 1:
+                intent = new Intent(this, CustomMatcherActivity.class);
+                break;
+            case 2:
+                intent = new Intent(this, LongListActivity.class);
+                break;
+            case 3:
+                intent = new Intent(this, IdlingResourcesActivity.class);
+                break;
+            case 4:
+                intent = new Intent(this, ImageViewerActivity.class);
+                break;
+            case 5:
+                intent = new Intent(this, SuggestActivity.class);
+                break;
+            case 6:
+                intent = new Intent(this, RecyclerSampleActivity.class);
+                break;
+            case 7:
+                intent = new Intent(this, WebViewActivity.class);
+                break;
+        }
+        startActivity(intent);
     }
 
     public static class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeHolder>{
