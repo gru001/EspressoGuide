40ea0fb77315239436b4cec2a3236e081f59cd44
pranit <pranitsawant01@gmail.com>
2018-02-01 16:11:18 +0530

    added divider

--- app/src/main/java/com/example/pranit/espressoguide/MainActivity.java
+++ app/src/main/java/com/example/pranit/espressoguide/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.pranit.espressoguide;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnItemClickListener {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private RecyclerView recvHome;
     private String[] list = new String[]{"Basic", "Custom Matcher", "Data Adapter",
             "Idling Resource", "Intent Advanced", "Intent Basic", "Multi-Process", "Multi-window", "RecyclerView", "WebView"};
@@ -18,8 +22,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recvHome = findViewById(R.id.recv_home);
+        recvHome.setLayoutManager(new LinearLayoutManager(this));
+        recvHome.setHasFixedSize(true);
+        recvHome.setAdapter(new HomeAdapter(list, this));
+        recvHome.addItemDecoration(new DividerItemDecoration(this));
+    }
 
-
+    @Override
+    public void onItemClick(int position) {
+        Log.i(TAG, "onItemClick: "+position);
     }
 
     public static class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeHolder>{
@@ -33,11 +44,13 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public HomeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
+            View rootView = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout, parent, false);
+            return new HomeHolder(rootView);
         }
 
         @Override
         public void onBindViewHolder(HomeHolder holder, int position) {
+            holder.lblTitle.setText(array[position]);
             holder.bind(position, listener);
         }
 
